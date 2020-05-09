43b146d9b8f18a3da04f991c52f777e137e5cd15
Luis Valverde <lvalverdeb@gmail.com>
2020-05-09 08:10:59 -0600

    Added HTTP Methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Rest API View"""
+    serializer_class = serializers.HelloSerializer
     def get(self,request, format=None):
         """returns a list of APIView features"""
         an_apiview =[
@@ -12,3 +16,24 @@ class HelloApiView(APIView):
             "Is mapped manually to URLs",
         ]
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+    def post(self,request):
+        """Create a hello with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def put(self,request, pk=None):
+        """handle updating an object"""
+        return Response({'method':'PUT'})
+
+    def patch(self,request,pk=None):
+        """handle a partial update of an object"""
+        return Response({'method':'PATCH'})
+    def delete(self,request,pk=None):
+        """Delete an object"""
+        return Response({'message':'DELETE'})
