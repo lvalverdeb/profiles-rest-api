bf7690240c96023134bcf368e6b323ee57a38ad5
Luis Valverde <lvalverdeb@gmail.com>
2020-05-09 11:13:30 -0600

    Added Authentication and Permissions to API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,6 +4,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profile',views.UserProfileViewSet)  # no basename required since we have specified a queryset in the viewset
 
 from profiles_api import views
 
@@ -11,3 +12,5 @@ urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path('', include(router.urls) )
 ]
+
+#Monitoring And Transport Through Intelligent Linking and Directed Assistance
